309a9c37a916bc1558329d41fd633c8ff18e7364
UGrupo7 <snomesque@uninorte.edu.co>
2022-10-11 19:48:40 -0500

    copia sprint 3

--- app/auth.py
+++ app/auth.py
@@ -6,7 +6,7 @@ from . import utils
 from email.message import EmailMessage
 import smtplib
 
-from flask import (
+from flask import ( 
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
